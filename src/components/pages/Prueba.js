8ab0566463c70a348a32ae102c21337909047940
Alberto Peral <info@peralstudio.com>
2021-01-17 22:26:39 +0100

    Update Prueba.js

--- src/components/pages/Prueba.js
+++ src/components/pages/Prueba.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../../App.css';
 import Pruebapage from '../Pruebapage';
-import BurgerButtons from '../BurgerButton/BurgerButton';
 /* import PagPrincipal from '../PagPrincipal'; */
 
 export default function Prueba() {
     return <>
     
     <Pruebapage />
-    <BurgerButtons />
     {/* <PagPrincipal /> */}
     
   </>
